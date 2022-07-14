c62e93d39ffec3b1fdc80d3aac0f3dc4f17e54c0
Fogelzone <fogelzone@gmail.com>
2022-07-15 00:00:21 +0300

    1

--- src/BuyMe/Pages/HomePage.java
+++ src/BuyMe/Pages/HomePage.java
@@ -1,4 +1,40 @@
 package Pages;
 
-public class HomePage {
+import Data.BasePage;
+import org.openqa.selenium.By;
+
+public class HomePage extends BasePage {
+
+    public static void searchGift() throws InterruptedException{
+        pickPricePoint();
+        pickRegion();
+        pickCategory();
+        pressFindGiftBtn();
+    }
+
+    public static void pickPricePoint() throws InterruptedException{
+        clickElement(By.className("notSigned"));
+        sendKeysToElement(By.cssSelector("input[placeholder='מייל']"), "Auto@project.com");
+        sendKeysToElement(By.cssSelector("input[placeholder='סיסמה']"), "Aa123456");
+        clickElement(By.cssSelector("button[type='submit']"));
+        Thread.sleep(5000);
+        clickElement(By.cssSelector("span[alt='סכום']"));
+        clickElement(By.cssSelector("li[value='1']"));
+    }
+
+    public static void pickRegion(){
+        clickElement(By.cssSelector("span[alt='אזור']"));
+        clickElement(By.cssSelector("li[value='13']"));
+    }
+
+    public static void pickCategory(){
+        clickElement(By.cssSelector("span[alt='קטגוריה']"));
+        clickElement(By.cssSelector("li[value='409']"));
+    }
+
+    public static void pressFindGiftBtn(){
+        clickElement(By.cssSelector("a[href='https://buyme.co.il/search?budget=1&category=409&region=13']"));
+    }
+
+
 }
--- src/BuyMe/Runner/BuyMeTest.java
+++ src/BuyMe/Runner/BuyMeTest.java
@@ -1,15 +1,22 @@
 package Runner;
 
+import Pages.HomePage;
 import Pages.RegisterPage;
 import org.testng.annotations.Test;
 
 
 public class BuyMeTest {
 
+//    @Test
+//    public void test01_Register() {
+//        RegisterPage registerPage = new RegisterPage();
+//        RegisterPage.register();
+//    }
+
     @Test
-    public void test01_Register() {
-        RegisterPage registerPage = new RegisterPage();
-        RegisterPage.register();
+    public void test02_SearchGift() throws InterruptedException {
+        HomePage homePage = new HomePage();
+        HomePage.searchGift();
     }
 
 }
